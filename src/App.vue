a7352eef2b0ab97eec504bf6870eeee2bd56fa4c
byrantwithyou <byrantwithyou@163.com>
2019-01-22 12:21:43 -0500

    add vuex and element successfully

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <router-view></router-view>
+  <el-button type="primary" icon="el-icon-edit"></el-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from "@/router";
-import './plugins/element.js'
+import store from "@/store";
+import './plugins/element.js';
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
+  store,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
