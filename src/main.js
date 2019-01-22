ad1bd3d513a7d6a411e7fe14cc43c96dab782958
byrantwithyou <byrantwithyou@163.com>
2019-01-22 12:07:06 -0500

    add element ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
-
-Vue.config.productionTip = false;
+import router from "@/router";
+import './plugins/element.js'
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,11 @@ import Router from "vue-router";
 import HelloWorld from "@/components/HelloWorld";
 
 Vue.use(Router);
-const routes = {
-  path: "/hello",
-  component: HelloWorld
-}
+const routes = [{
+    path: "/",
+    component: HelloWorld
+  }
+]
 
 export default new Router({
     routes
