f7c7f130517818b1748ef282b2f75e682ba5d5e7
byrantwithyou <byrantwithyou@163.com>
2019-01-23 17:31:58 -0500

    vuex is ready

--- src/store/modules/circuits.js
+++ src/store/modules/circuits.js
@@ -17,10 +17,12 @@ const mutations = {
     })
   },
   removeComponent(state, payload) {
-    let ind = state.circuits[payload.socketId].findIndex( (element) => element.componentId == payload.componentId)
-    if (ind != -1) {
-      state.circuits[payload.socketId].splice(ind, 1);
-    }
+    if (state.circuits[payload.socketId]){
+      let ind = state.circuits[payload.socketId].findIndex( (element) => element.componentId == payload.componentId)
+      if (ind != -1) {
+        state.circuits[payload.socketId].splice(ind, 1);
+      }
+    } 
   }
 };
 
