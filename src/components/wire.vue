dc8afaf4836244cabeec14dfd368781ccd08da6a
byrantwithyou <byrantwithyou@163.com>
2019-01-25 11:17:28 -0500

    all for one

--- src/components/wire.vue
+++ src/components/wire.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div :style="{position: position, top: top, left: left}">
     <svg>
-      <line x1="0" y1="0" x2="200" y2="200" style="stroke:rgb(255,0,0);stroke-width:2" />
+      <line x1="0" y1="0" :x2="200" :y2="200" style="stroke:rgb(255,0,0);stroke-width:2" />
     </svg>
   </div>
 </template>
@@ -11,9 +11,19 @@ export default {
   name: 'wire',
   data() {
     return {
+      position: "absolute",
     }
   },
   computed: {
+    left: function () {
+      let posx = Number(this.posx0);
+      let left = (posx - 1) * 1.51 + 2.25;
+      return left + "%";
+
+    },
+    top: function () {
+      return "1%";
+    }
   },
   props: {
     posx0: String,
@@ -24,7 +34,12 @@ export default {
   components: {
   },
   methods: {
-
+    posx2left: function (posx) {
+      let left = (posx - 1) * 1.51 + 2.25;
+      /* eslint-disable */
+      console.log(left);
+      return left;
+    }
   }
   
 }
